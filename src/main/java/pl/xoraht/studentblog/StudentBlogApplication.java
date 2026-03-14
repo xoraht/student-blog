bcb9bfc774106959f6ea24921539325ba461fce3
xorah <xorah@XORAHT>
2026-03-14 18:59:12 +0100

    Initial commit

---
+++ src/main/java/pl/xoraht/studentblog/StudentBlogApplication.java
@@ -0,0 +1,13 @@
+package pl.xoraht.studentblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentBlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentBlogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/xoraht/studentblog/StudentBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.xoraht.studentblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentBlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
