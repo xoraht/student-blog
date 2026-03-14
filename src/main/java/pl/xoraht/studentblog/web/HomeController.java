fc23266110422a96d6d54c998a0ddbea73551675
xorah <xorah@XORAHT>
2026-03-14 19:02:51 +0100

    Add home page (Thymeleaf) with basic layout

---
+++ src/main/java/pl/xoraht/studentblog/web/HomeController.java
@@ -0,0 +1,17 @@
+package pl.xoraht.studentblog.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.time.LocalDate;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String home(Model model) {
+        model.addAttribute("today", LocalDate.now());
+        return "home";
+    }
+}
